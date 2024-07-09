8ceadcaff3ad507cfb010d8b32166fec437ededf
Know167 <191200116035@asoit.edu.in>
2024-07-09 01:15:12 -0400

    using NextUI navbar for Navigation

--- Components/Navigation.js
+++ Components/Navigation.js
@@ -1,15 +1,95 @@
 import { useSession, signIn } from "next-auth/react";
-import Link from "next/link";
 
 import UserBio from "@/Components/UserBio";
+import React from "react";
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    NavbarMenuToggle,
+    NavbarMenu,
+    NavbarMenuItem,
+    Link,
+    Button,
+} from "@nextui-org/react";
 
 const Navigation = (props) => {
     const { data: session } = useSession();
-    
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+    const menuItems = [
+        "Why GiT Monger?",
+        "Use Cases",
+        "Pricing",
+        "Support",
+        "Blog",
+    ];
     return (
         <>
+            {!session && (
+                <Navbar onMenuOpenChange={setIsMenuOpen} isBordered className="bg-teal-400 rounded">
+                    <NavbarContent>
+                        <NavbarMenuToggle 
+                            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+                            className="sm:hidden text-xl"
+                        />
+                        <NavbarBrand>
+                            <p className="py-2 md:text-nowrap text-white text-xl lg:text-2xl font-bold font-['VT323'] items-center lg:mx-6">
+                                Git Monger
+                            </p>
+                        </NavbarBrand>
+                    </NavbarContent>
+
+                    <NavbarContent
+                        className="hidden sm:flex gap-4"
+                        justify="center">
+                        {menuItems?.map((item) => (
+                            <NavbarItem key={item}>
+                                <Link color="foreground" href="#">
+                                    {item}
+                                </Link>
+                            </NavbarItem>
+                        ))}
+                    </NavbarContent>
+                    <NavbarContent justify="end">
+                        <NavbarItem className="hidden lg:flex">
+                            <Link href="#">Login</Link>
+                        </NavbarItem>
+                        <NavbarItem>
+                            <Button
+                                as={Link}
+                                color="primary"
+                                href="#"
+                                variant="flat">
+                                Sign Up
+                            </Button>
+                        </NavbarItem>
+                    </NavbarContent>
+                    <NavbarMenu>
+                        {menuItems.map((item, index) => (
+                            <NavbarMenuItem key={`${item}-${index}`}>
+                                <Link
+                                    color={
+                                        index === 2
+                                            ? "primary"
+                                            : index === menuItems.length - 1
+                                            ? "danger"
+                                            : "foreground"
+                                    }
+                                    className="w-full"
+                                    href="#"
+                                    size="lg">
+                                    {item}
+                                </Link>
+                            </NavbarMenuItem>
+                        ))}
+                    </NavbarMenu>
+                </Navbar>
+            )}
+
             {session && (
-                <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6 rounded-lg">
+                <Navbar className="flex items-center justify-between flex-wrap bg-teal-500 p-6 rounded-lg">
                     <div className="flex items-center flex-shrink-0 text-white mr-8">
                         <span className="font-semibold text-2xl tracking-tight">
                             GiT Monger
@@ -33,7 +113,7 @@ const Navigation = (props) => {
                             )}
                         </div>
                     </div>
-                </nav>
+                </Navbar>
             )}
             {props.children}
         </>
