be082be17d9433a7c80d9cf2afb920994824da00
Know167 <191200116035@asoit.edu.in>
2024-08-24 17:03:50 -0400

    changing router behaviour on logout

--- Components/UserBio.js
+++ Components/UserBio.js
@@ -10,7 +10,7 @@ const UserBio = (props) => {
     const logout = (e) => {
         e.preventDefault();
         signOut();
-        router.push("/");
+        router.push("");
     };
     const toggleMenu = () => {
         console.log("toggling");
