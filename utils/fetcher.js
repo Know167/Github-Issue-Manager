3561c177e8ffea379cea8942f8a612a86ae9f089
Know167 <90709908+Know167@users.noreply.github.com>
2025-04-05 17:45:08 -0400

    Update fetcher.js
    making code readable

--- utils/fetcher.js
+++ utils/fetcher.js
@@ -1,16 +1,20 @@
 const fetcher = async (url, token, data) => {
+  try {
     const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            token: token,
-            ...data,
-        }),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: token,
+        ...data,
+      }),
     });
 
     return response;
+  } catch (err) {
+     console.log(err);  
+   }
 };
 
 export default fetcher;
