cefabe0479918729ddd23f9fae3270ea665970a9
Know167 <90709908+Know167@users.noreply.github.com>
2025-04-06 15:05:08 -0400

    Update UserDataProvider.js
    moving the reducer to the top for better readability

--- store/userDataProvider.js
+++ store/userDataProvider.js
@@ -1,29 +1,25 @@
 import { useReducer } from "react";
 import { UserDataContext, initialState } from "./userDataContext";
 
-
-const UserDataProvider = (props) => {
-    const userDataReducer = (userDataState, action) => {
-        switch (action.type) {
-
-            
-                
-            case "HANDLE_REPO_LIST": {
-                return {
-                    ...userDataState,
-                    repoList: action.repoList,
-                };
-            }
+const userDataReducer = (userDataState, action) => {
+    switch (action.type) {
+        case "HANDLE_REPO_LIST": {
+            return {
+                ...userDataState,
+                repoList: action.repoList,
+            };
         }
-    };
-   
+    }
+};
+const UserDataProvider = (props) => {
+    const [userDataState, dispatch] = useReducer(userDataReducer, initialState);
+
     const handleRepoList = (repoList) => {
         dispatch({
             type: "HANDLE_REPO_LIST",
             repoList: repoList,
         });
     };
-    const [userDataState, dispatch] = useReducer(userDataReducer, initialState);
     return (
         <UserDataContext.Provider
             value={{
