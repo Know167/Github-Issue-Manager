1002d4335528f1c728179959823c6f6bf3a40ce5
Know167 <90709908+Know167@users.noreply.github.com>
2025-04-16 18:25:39 -0400

    Update add-git-issue.js
    rewriting the api call for form data handling as well as responding appropriately for errors

--- pages/api/add-git-issue.js
+++ pages/api/add-git-issue.js
@@ -1,23 +1,35 @@
 import { Octokit } from "octokit";
 
 const handler = async (req, res) => {
-    const token = req.body.token;
-    const title = req.body.title;
-    const desc = req.body.desc;
-    const repoowner = req.body.owner;
-    const reponame = req.body.reponame;
-    const octokit = new Octokit({
-        auth: token,
-    });
-    const response = await octokit.request(
-        "POST /repos/{owner}/{repo}/issues",
-        {
-            owner: repoowner,
-            repo: reponame,
-            title: title,
-            body: desc,
+    try {
+        if (
+            !req.body.token ||
+            !req.body.title ||
+            !req.body.owner ||
+            !req.body.reponame
+        ) {
+            return res.status(400).json({ error: "Missing required fields" });
         }
-    );
-    res.json(response);
+        const token = req.body.token;
+        const title = req.body.title;
+        const desc = req.body.desc;
+        const repoowner = req.body.owner;
+        const reponame = req.body.reponame;
+        const octokit = new Octokit({
+            auth: token,
+        });
+        const response = await octokit.request(
+            "POST /repos/{owner}/{repo}/issues",
+            {
+                owner: repoowner,
+                repo: reponame,
+                title: title,
+                body: desc,
+            }
+        );
+        res.json(response);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
 };
 export default handler;
